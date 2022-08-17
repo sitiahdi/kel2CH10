0dd584e7ffa85078175c089a28d7f50bbb29c177
Mamang007 <abdan.sm@gmail.com>
2022-08-17 09:02:38 +0700

    Update Game Page

--- pages/game-page.js
+++ pages/game-page.js
@@ -8,10 +8,50 @@ import Gunting from '../public/image/Gunting.png';
 import Logo from '../public/image/Logo.png';
 import Refresh from '../public/image/Refresh.png';
 import { style } from 'dom-helpers';
+import { useId, useState } from 'react';
 
 
 
 function GamePage() {
+    function handleClick() {
+        window.location.reload();
+    }
+
+    function playerOnClick() {
+        // document.getElementById('playerHand').style.backgroundColor = "#9C835F";
+        var element = document.getElementById('playerWin');
+        var value = element.innerHTML;
+
+        ++value;
+
+        console.log(value);
+        // document.getElementById('playerBatu').style.backgroundColor= 'red'
+        document.getElementById('playerWin').innerHTML = value;
+    }
+    function handOnClick() {
+        var el = useId('playerHand');
+        var value = el.
+
+        if (el.value == 1) {
+            console.log('batu')
+        } else if (el.value == 2) {
+            console.log('kertas')
+        } else if (el.value == 3) {
+            console.log('gunting')
+        }
+    }
+    function comOnClick() {
+        var element = document.getElementById('comWin');
+        var value = element.innerHTML;
+
+        ++value;
+
+        console.log(value);
+        document.getElementById('comWin').innerHTML = value;
+    }
+
+    //
+
     return (
         <div 
         style={{ backgroundColor:'#9C835F', height:'100vh', paddingLeft:'100px' }}>
@@ -24,32 +64,54 @@ function GamePage() {
                 </div>
             </div>
             <div style={{ display:'flex', justifyContent:'center', fontSize:'20px'}}>
-                <div style={{backgroundColor:'yellowgreen', width:'200px', textAlign:'center' }} className='rounded-full'>
-                    Score 0-0
+                <div 
+                style={{ display:'flex', justifyContent:'center', backgroundColor:'yellowgreen', width:'200px' }} 
+                className='rounded-full'>
+                    <h1 id='playerWin'>0</h1>
+                    -
+                    <h1 id='comWin'>0</h1>
                 </div>
             </div>
             <div style={{ display:'flex', justifyContent:'space-evenly' }}>
-                <div style={{width:'100px'}}>
+                <div 
+                style={{width:'100px'}}>
                     <div style={{ display:'flex', justifyContent:'space-around' }}>Player 1</div>
-                    <div style={{ padding:'20px' }}>
+                    <div 
+                    id='playerHand playerBatu'
+                    className='bg-#9C835F-500 hover:bg-red-700 rounded-full'
+                    style={{ padding:'20px' }}
+                    onClick={ playerOnClick, handOnClick }
+                    value='1'>
                         <Image src={Batu} />
                     </div>
-                    <div style={{ padding:'20px' }}>
+                    <div 
+                    id='playerHand playerKertas'
+                    className='bg-#9C835F-500 hover:bg-red-700 rounded-full'
+                    style={{ display:"block", padding:'20px' }}
+                    onClick={ playerOnClick, handOnClick }
+                    value='2'>
                         <Image src={Kertas} />
                     </div>
-                    <div style={{ padding:'20px' }}>
+                    <div 
+                    id='playerHand playerGunting'
+                    className='bg-#9C835F-500 hover:bg-red-700 rounded-full'
+                    style={{ padding:'20px' }}
+                    onClick={ playerOnClick, handOnClick }
+                    value='3'>
                         <Image src={Gunting} />
                     </div>
                 </div>
                 <div style={{ display:'flex', alignItems:'center', }}>
                     <div>
                         <h1 style={{ fontSize:'80px', color:'tomato', fontWeight:'bold ' }}>VS</h1>
-                        <div style={{ display:'flex', justifyContent:'center' }}>
+                        <div onClick={handleClick} style={{ display:'flex', justifyContent:'center' }}>
                             <Image src={Refresh} />
                         </div>
                     </div>
                 </div>
-                <div style={{width:'100px'}}>
+                <div 
+                onClick={comOnClick}
+                style={{width:'100px'}}>
                     <div style={{ display:'flex', justifyContent:'space-around' }}>Com</div>
                     <div style={{ padding:'20px' }}>
                         <Image src={Batu} />
