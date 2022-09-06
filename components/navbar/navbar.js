983f75b13d9ceb4d2637ce1475a2bd43d88cc5b5
azhar <azhar.rachman@Hotmail.com>
2022-09-06 19:24:11 +0700

    addpdfhandling

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -17,28 +17,29 @@ function Navbar() {
   const db = firebaseApp.firestore();
 
   React.useEffect(() => {
-    const cookie = getCookie('token');
+    const cookie = getCookie("token");
 
     if (!cookie) {
-      return
+      return;
     }
     const decoded = jwtDecode(cookie);
-        
-    db.collection('users').get().then(querrySnapShot => {
-        
-      if (!querrySnapShot) {
-        throw Error('failed to get data')
-      }
-      
-      querrySnapShot.forEach(e => {
-        if (e.data().userUid === decoded.user_id) {
-          dispatch(setName(e.data().username));
+
+    db.collection("users")
+      .get()
+      .then((querrySnapShot) => {
+        if (!querrySnapShot) {
+          throw Error("failed to get data");
         }
+
+        querrySnapShot.forEach((e) => {
+          if (e.data().userUid === decoded.user_id) {
+            dispatch(setName(e.data().username));
+          }
+        });
+      })
+      .catch((err) => {
+        alert(err);
       });
-    }).catch(err => {
-      alert(err);
-    });
-    
   }, []);
 
   const handleLogout = () => {
@@ -46,7 +47,12 @@ function Navbar() {
       .then(() => {
         dispatch(setLogout);
         document.cookie.split(";").forEach((c) => {
-          document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
+          document.cookie = c
+            .replace(/^ +/, "")
+            .replace(
+              /=.*/,
+              "=;expires=" + new Date().toUTCString() + ";path=/"
+            );
         });
         dispatch(selectName);
         window.location.href = "/";
@@ -56,16 +62,32 @@ function Navbar() {
   };
   return (
     <div>
-      <nav className={`navbar fixed-top navbar-expand-lg navbar-dark py-lg-3 px-sm-5 px-lg-2 px-3 ${style.navbar}`}>
+      <nav
+        className={`navbar fixed-top navbar-expand-lg navbar-dark py-lg-3 px-sm-5 px-lg-2 px-3 ${style.navbar}`}
+      >
         <div className="container-fluid">
           <Link href="/">
-            <a className={`navbar-brand ms-lg-5 ps-lg-5 me-lg-5 ${style.brand}`}>PLAY</a>
+            <a
+              className={`navbar-brand ms-lg-5 ps-lg-5 me-lg-5 ${style.brand}`}
+            >
+              PLAY
+            </a>
           </Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className={`navbar-nav text center ms-xl-auto ps-lg-auto me-lg-auto ${style.link}`}>
+            <ul
+              className={`navbar-nav text center ms-xl-auto ps-lg-auto me-lg-auto ${style.link}`}
+            >
               <li className="nav-item ms-lg-4">
                 <Link href="/game-list">
                   <a className={`${style.link}`}>GAME</a>
@@ -86,22 +108,35 @@ function Navbar() {
                   <a className={`${style.link}`}>NEWS</a>
                 </Link>
               </li>
+              <li className="nav-item ms-lg-4">
+                <Link href="/specification">
+                  <a className={`${style.link}`}>SPECIFICATION</a>
+                </Link>
+              </li>
             </ul>
             {user.payload.name.name ? (
-              <ul className={`navbar-nav text center me-lg-5 pe-lg-5 ${style.link}`}>
+              <ul
+                className={`navbar-nav text center me-lg-5 pe-lg-5 ${style.link}`}
+              >
                 <li className="nav-item">
                   <Link href="/profile">
-                    <a className={`${style.link} text-warning`}>{user.payload.name.name}</a>
+                    <a className={`${style.link} text-warning`}>
+                      {user.payload.name.name}
+                    </a>
                   </Link>
                 </li>
                 <li className="nav-item">
                   <button onClick={() => handleLogout()}>
-                    <a className={`${style.link} text-danger ms-lg-4`}>LOGOUT</a>
+                    <a className={`${style.link} text-danger ms-lg-4`}>
+                      LOGOUT
+                    </a>
                   </button>
                 </li>
               </ul>
             ) : (
-              <ul className={`navbar-nav text center me-lg-5 pe-lg-5 ${style.link}`}>
+              <ul
+                className={`navbar-nav text center me-lg-5 pe-lg-5 ${style.link}`}
+              >
                 <li className="nav-item">
                   <Link href="/Register">
                     <a className={`${style.link}`}>REGISTER</a>
